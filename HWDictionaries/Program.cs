0076ae240556db10fe99e038231fcfeb64a6c7f6
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2023-07-27 13:32:18 -0500

    Add project files.

---
+++ HWDictionaries/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Dictionary<int, string> employees = new Dictionary<int, string>();
+
+employees[100] = "Bob";
+employees[101] = "Tad";
+employees[102] = "Tim";
+employees[103] = "Billy";
+
+Console.WriteLine("Hello, to look up an employee enter a number between 100 and 103");
+string employeeIdText = Console.ReadLine();
+
+if (int.TryParse(employeeIdText, out int employeeID) && employeeID >= 100 && employeeID <= 103)
+{
+    Console.WriteLine($"Employee {employeeID} is {employees[employeeID]}");
+}
+else
+{
+    Console.WriteLine($"There is no employee with an id of {employeeIdText}");
+}
+
+
+
